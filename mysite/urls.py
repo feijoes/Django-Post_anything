48b361e71bd9db66a0393bee1061fcd72cc2c6bb
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-02-28 02:37:37 -0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('idk.urls'))
+    path('main/', include('idk.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
