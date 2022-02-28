6d6df157181a83d45fa1d2a1873fd6e1f2631d85
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-02-28 01:58:39 -0300

    Update settings.py

--- paths/mysite/settings.py
+++ paths/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+94h5og7p_xsf+ifjif5xltpxbqdul^k(f1v&2tgsnp@0h&kqb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'postanything.com']
 
 
 INSTALLED_APPS = [
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
