7eb8d3f01f468cfd2b584111bd743e942f993333
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-02-28 02:22:23 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,9 +113,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-
+BOOTSTRAP5 = {
+    'include_jquery': True,
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
